dceea5a9557ed56716e674d5c14a92b48dfd23e1
ivan <ivan@localhost.localdomain>
2020-09-09 11:32:16 +0400

    Configuration properties

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,14 +1,32 @@
 package com.vega.springit;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import org.springframework.context.annotation.Bean;
+
+import com.vega.springit.config.SpringitProperties;
+
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+    @Autowired
+    private SpringitProperties springitProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("Welcome to Springit!");
 	}
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            System.out.println("Welcome message: " + springitProperties.getWelcomeMsg());
+        };
+    }
 }
